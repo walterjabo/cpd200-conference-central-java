f2d53db4aa69d50def75206eb1ce619f51318c40
Sharif Salah <sharif.salah@gmail.com>
2015-12-07 09:47:49 +0000

    Removed unused constants
    Removed constants used later in the course for memcache, Android and
    iOS. These should be added back in manually later in the course.

--- default/src/main/java/com/google/training/cpd200/conference/Constants.java
+++ default/src/main/java/com/google/training/cpd200/conference/Constants.java
@@ -7,11 +7,6 @@ import com.google.api.server.spi.Constant;
  */
 public class Constants {
     public static final String WEB_CLIENT_ID = "replace with your client id";
-    public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
-    public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
-    public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
     public static final String EMAIL_SCOPE = Constant.API_EMAIL_SCOPE;
     public static final String API_EXPLORER_CLIENT_ID = Constant.API_EXPLORER_CLIENT_ID;
-
-    public static final String MEMCACHE_ANNOUNCEMENTS_KEY = "RECENT_ANNOUNCEMENTS";
 }
