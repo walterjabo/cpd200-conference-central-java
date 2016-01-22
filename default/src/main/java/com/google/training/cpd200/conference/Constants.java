ac2487469b286403db82196bf7cfd57dba014283
Sharif Salah <sharif.salah@gmail.com>
2016-01-22 22:34:26 +0000

    Revert "Update Constants.java"
    This reverts commit b81a6f06190ecf19f469941e41ff1026133452d9.

--- default/src/main/java/com/google/training/cpd200/conference/Constants.java
+++ default/src/main/java/com/google/training/cpd200/conference/Constants.java
@@ -9,5 +9,4 @@ public class Constants {
     public static final String WEB_CLIENT_ID = "replace with your client id";
     public static final String EMAIL_SCOPE = Constant.API_EMAIL_SCOPE;
     public static final String API_EXPLORER_CLIENT_ID = Constant.API_EXPLORER_CLIENT_ID;
-    public static final String MEMCACHE_ANNOUNCEMENTS_KEY = "RECENT_ANNOUNCEMENTS";
 }
