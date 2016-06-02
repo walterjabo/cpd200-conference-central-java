4b9a240cc7c0b60c8e6cbed56989587fe7d338ab
Walter Jabo <walter.jabo@gmail.com>
2016-06-02 07:59:37 -0500

    conference persisted

--- default/src/main/java/com/google/training/cpd200/conference/spi/ConferenceApi.java
+++ default/src/main/java/com/google/training/cpd200/conference/spi/ConferenceApi.java
@@ -16,6 +16,9 @@ import com.google.training.cpd200.conference.form.ProfileForm;
 import com.google.training.cpd200.conference.form.ProfileForm.TeeShirtSize;
 import com.googlecode.objectify.Key;
 
+import com.googlecode.objectify.cmd.Query;
+import java.util.List;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -210,5 +213,68 @@ public class ConferenceApi {
         return conference;
     }
 
+
+    /**
+     * Queries against the datastore for all conferences and returns the result.
+     *
+     * @return A List of Conferences.
+     */
+    @ApiMethod(
+            name = "queryConferences",
+            path = "queryConferences",
+            httpMethod = HttpMethod.POST
+    )
+    public List<Conference> queryConferences() {
+        // Find all entities of type Conference
+        Query<Conference> query = ofy().load().type(Conference.class).order("name");
+    
+        return query.list();
+    }
+    
+    /**
+     * Returns a list of Conferences that the user created.
+     * In order to receive the websafeConferenceKey via the JSON params, uses a POST method.
+     *
+     * @param user A user who invokes this method, null when the user is not signed in.
+     * @return a list of Conferences that the user created.
+     * @throws UnauthorizedException when the user is not signed in.
+     */
+    @ApiMethod(
+            name = "getConferencesCreated",
+            path = "getConferencesCreated",
+            httpMethod = HttpMethod.POST
+    )
+    public List<Conference> getConferencesCreated(final User user) throws UnauthorizedException {
+        // If not signed in, throw a 401 error.
+        if (user == null) {
+            throw new UnauthorizedException("Authorization required");
+        }
+        String userId = user.getUserId();
+        Key<Profile> userKey = Key.create(Profile.class, userId);
+        return ofy().load().type(Conference.class)
+                .ancestor(userKey)
+                .list();
+    }
+
+    @ApiMethod(
+            name = "filterPlayground",
+            path = "filterPlayground",
+            httpMethod = HttpMethod.POST
+    )
+    public List<Conference> filterPlayground() {
+        Query<Conference> query = ofy().load().type(Conference.class);
+
+        // Filter on city
+        query = query.filter("city =", "Paris");
+
+        /*
+        TODO
+        add 2 filters:
+        1: city equals to Chicago
+        2: topic equals "Medical Innovations"
+        */
+
+        return query.list();
+    }
 
 }
